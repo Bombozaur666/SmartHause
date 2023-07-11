9130ed12fc60fc09f100aa641bc6e01feef1ae00
Bombo <bombozaur6@gmail.com>
2023-07-11 17:13:52 +0200

    update celery task

--- src/measurements/tasks.py
+++ src/measurements/tasks.py
@@ -3,18 +3,41 @@ from celery import shared_task
 from devices.models import Devices
 from results.models import HumidityResults, TempResults
 from datetime import datetime
-from httpx import Client, Timeout
+from httpx import AsyncClient, Timeout
 from asgiref.sync import async_to_sync
+
+
+async def async_temp_and_humidity():
+    bulk_temperature = []
+    bulk_humidity = []
+    content = []
+    time_now = datetime.now()
+    devices = Devices.objects.filter(active=True,
+                                     type=Devices.THERMAL_AND_HUMIDITY)
+    async with AsyncClient() as client:
+        async for device in devices:
+            response = await client.get(f'{device.protocol}://{device.ip_address}:{device.port}/state/extended')
+            result = response.json()
+            content.append(result)
+            temp = TempResults(device=device,
+                               created=time_now,
+                               temp_value=result['multiSensor']['sensors'][1]['value'],
+                               heat_index=result['multiSensor']['sensors'][4]['value'],
+                               )
+            hum = HumidityResults(device=device,
+                                  created=time_now,
+                                  humidity=result['multiSensor']['sensors'][0]['value'],
+                                  absolute_humidity=result['multiSensor']['sensors'][2]['value'], )
+            bulk_temperature.append(temp)
+            bulk_humidity.append(hum)
+
+    await TempResults.objects.abulk_create(bulk_temperature)
+    await HumidityResults.objects.abulk_create(bulk_humidity)
+
+    return content
 
 
 @shared_task(serializer='json', name="temp_and_humidity_task")
 def temp_and_humidity(*args, **kwargs):
-    bulk_save = []
-    time_now = datetime.now()
-    devices = Devices.objects.filter(active=True,
-                                     type=Devices.THERMAL_AND_HUMIDITY)
-    with Client() as client:
-        for device in devices:
-            response = client.get(f'{device.protocol}://{device.ip_address}:{device.port}/state')
-            bulk_save.append(response.content)
-    return {'state': bulk_save}
+    context = async_to_sync(async_temp_and_humidity)()
+    return context
