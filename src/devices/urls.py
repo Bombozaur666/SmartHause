cda2c638040a130b67b67879b9bd9ba4642d38d5
Bombo <bombozaur6@gmail.com>
2023-10-09 16:17:12 +0200

    configure urls

--- src/devices/urls.py
+++ src/devices/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import DeviceListActive
 
 app_name = 'device'
 
 urlpatterns = [
+    path("list/", DeviceListActive.as_view(), name="device-list-active")
 ]
