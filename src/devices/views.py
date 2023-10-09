590f0c1f1b7409341913d7ff1331081f6889c317
Bombo <bombozaur6@gmail.com>
2023-10-09 16:16:53 +0200

    create device list response

--- src/devices/views.py
+++ src/devices/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Devices
+from rest_framework import status
+from rest_framework import generics
+from django.shortcuts import get_list_or_404, get_object_or_404
+from .serializers import DeviceSerializer
+
 
 # Create your views here.
+
+class DeviceListActive(generics.ListAPIView):
+    model = Devices
+    serializer_class = DeviceSerializer
+    queryset = Devices.objects.filter(active=True)
